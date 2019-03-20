9d90b9f1799549839b6d63b966cb530ece7ff5a0
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-03-20 12:22:32 +0600

    add router

--- hw_77_frontend/src/App.js
+++ hw_77_frontend/src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
+import Switch from "react-router-dom/es/Switch";
+import Route from "react-router-dom/es/Route";
+import Messages from "./containers/Messages/Messages";
+import NewMessage from "./containers/NewMessage/NewMessage";
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <Switch>
+                    <Route path="/" exact component={Messages}/>
+                    <Route path="/messages/new" exact component={NewMessage}/>
+                </Switch>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
